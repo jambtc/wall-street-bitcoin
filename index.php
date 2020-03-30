ee468b8a12975eeba7abed73f172982ce6664fdd
jambtc <jambtc@gmail.com>
2020-03-30 13:24:08 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>wall street bitcoin</title>
+	<base href="/">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<style>
+.on {
+    background-color: yellow;
+}
+body {
+	font-family: monospace;
+}
+</style>
+
+<body>
+	<h1><i class="material-icons on">lightbulb_outline</i> wall street bitcoin</h1>
+
+	<iframe src="https://s.tradingview.com/widgetembed/?symbol=DJI&interval=180&symboledit=1&saveimage=0&toolbarbg=f1f3f6&studies=%5B%5D&hideideas=1&theme=Dark&style=1&timezone=Europe%2FRome&studies_overrides=%7B%7D&overrides=%7B%7D&enabled_features=%5B%5D&disabled_features=%5B%5D&locale=en" width="600" height="400" /></iframe>
+	<iframe src="https://s.tradingview.com/widgetembed/?symbol=SPY&interval=180&symboledit=1&saveimage=0&toolbarbg=f1f3f6&studies=%5B%5D&hideideas=1&theme=Dark&style=1&timezone=Europe%2FRome&studies_overrides=%7B%7D&overrides=%7B%7D&enabled_features=%5B%5D&disabled_features=%5B%5D&locale=en" width="600" height="400" /></iframe>
+	<iframe src="https://s.tradingview.com/widgetembed/?symbol=BITSTAMP:BTCUSD&interval=180&symboledit=1&saveimage=0&toolbarbg=f1f3f6&studies=%5B%5D&hideideas=1&theme=Dark&style=1&timezone=Europe%2FRome&studies_overrides=%7B%7D&overrides=%7B%7D&enabled_features=%5B%5D&disabled_features=%5B%5D&locale=en" width="1200" height="400" /></iframe>
+</body>
